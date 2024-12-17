e1d50b07907f76a5ef3e639cec967a0feba10cc3
OwenDiBacco <166024086+OwenDiBacco@users.noreply.github.com>
2024-12-17 15:58:33 -0500

    Removed sensitive information

--- Create_Notes_From_AI.py
+++ Create_Notes_From_AI.py
@@ -1,14 +1,3 @@
-# OpenAI requires monthly billing
-# Gemmini "free tier" is offered through the API service with lower rate limits for testing purposes
-# Project ID: "gemini-prompter-439417"
-# Gemini API Key: 'AIzaSyC7w6TBQQIE4xLDcsrOgnviJ4hHE-eLTdw'
-
-# PS C:\Users\CMP_OwDiBacco> setx OPENAI_API_KEY "API-Key" 
-
-# openai.api_key = "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" 
-# Owen's Key ^ 
-# Can only see key after you create it
-
 import os
 import google.generativeai as genai
 from dotenv import load_dotenv
